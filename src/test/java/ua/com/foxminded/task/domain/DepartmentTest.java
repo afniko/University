4bde0be2cf407bf9a2d0b8fe137e813c75afa845
afniko <afniko@gmail.com>
2019-05-23 15:51:14 +0300

    test(repository classes): remove stack owerflow #dev

---
+++ src/test/java/ua/com/foxminded/task/domain/DepartmentTest.java
@@ -0,0 +1,21 @@
+package ua.com.foxminded.task.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import ua.com.foxminded.task.domain.repository.DepartmentModelRepository;
+import ua.com.foxminded.task.domain.repository.TestModel;
+
+@RunWith(JUnitPlatform.class)
+class DepartmentTest {
+
+    @Test
+    void test() {
+        Department departmentTest = DepartmentModelRepository.getModel(TestModel.MODEL_1);
+        System.out.println(departmentTest.getGroups());
+        System.out.println(departmentTest.getTeachers());
+        System.out.println("\n test department");
+    }
+
+}
