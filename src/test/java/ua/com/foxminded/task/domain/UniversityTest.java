7e048d5b768d955e30de5c8b7fc595356eec95ca
afniko <afniko@gmail.com>
2019-05-24 12:15:13 +0300

    test(UniversityTest): add informations #dev

--- src/test/java/ua/com/foxminded/task/domain/UniversityTest.java
+++ src/test/java/ua/com/foxminded/task/domain/UniversityTest.java
@@ -19,15 +19,14 @@ public class UniversityTest {
         University university = UniversityModelRepository.getModel(TestModel.MODEL_1);
         System.out.println(university.getTimetable().getTimetableItems());
         System.out.println();
-        System.out.println(university.getTimetable().getTimetableItems().get(0).getGroups().get(0).getStudents());
-        System.out.println();
         Student student = StudentModelRepository.getModel(TestModel.MODEL_1);
-        System.out.println("Student from group : "+ student.getGroup());
         Date startPeriod = Date.valueOf("2019-01-20");
         Date endPeriod = Date.valueOf("2019-01-22");
+        
         Timetable timetable = university.getTimetable().findPeriod(student, startPeriod, endPeriod);
         System.out.println(timetable.getTimetableItems());
-
+// TODO візуально перевіряю що дійсно розклад містить лише ті позиції які призначені для цього студента
+//        та у встановлений період. Дороблю цю перевірку через Mickito
         System.out.println("\n test university student \n");
     }
     
@@ -39,9 +38,11 @@ public class UniversityTest {
         Teacher teacher = TeacherModelRepository.getModel(TestModel.MODEL_1);
         Date startPeriod = Date.valueOf("2019-01-20");
         Date endPeriod = Date.valueOf("2019-01-22");
+        
         Timetable timetable = university.getTimetable().findPeriod(teacher, startPeriod, endPeriod);
         System.out.println(timetable.getTimetableItems());
-
+// TODO візуально перевіряю що дійсно розклад містить лише ті позиції які призначені для цього вчителя
+//      та у встановлений період. Дороблю цю перевірку через Mickito
         System.out.println("\n test university teacher \n");
     }
 }
