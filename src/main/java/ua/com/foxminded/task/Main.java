bb28ba9442768335876a1c1904e3e4a86104bd9b
Mykola Afanasiev <afniko@gmail.com>
2019-07-16 22:56:41 +0300

    file(flyway.properties): #task10

--- src/main/java/ua/com/foxminded/task/Main.java
+++ src/main/java/ua/com/foxminded/task/Main.java
@@ -1,14 +1,9 @@
 package ua.com.foxminded.task;
 
-import ua.com.foxminded.task.dao.AuditoryTypeDao;
-import ua.com.foxminded.task.dao.impl.AuditoryTypeDaoImpl;
-import ua.com.foxminded.task.domain.AuditoryType;
-
 public class Main {
 
     public static void main(String[] args) {
 
-      
     }
 
 }
