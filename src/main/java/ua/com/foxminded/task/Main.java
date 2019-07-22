8699ed513655fecf33100c276a415fec369360b8
Mykola Afanasiev <afniko@gmail.com>
2019-07-22 17:49:35 +0300

    perf(V2.1__create_tables): delete file in test/resources folder #task10

--- src/main/java/ua/com/foxminded/task/Main.java
+++ src/main/java/ua/com/foxminded/task/Main.java
@@ -1,9 +1,48 @@
 package ua.com.foxminded.task;
 
+import java.sql.Date;
+
+import ua.com.foxminded.task.dao.DaoFactory;
+import ua.com.foxminded.task.dao.GroupDao;
+import ua.com.foxminded.task.dao.impl.GroupDaoImpl;
+import ua.com.foxminded.task.domain.Group;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static GroupDao groupDao;
+    private static final Group GROUP1 = getModel1();
+    private static final Group GROUP2 = getModel2();
+    private static final Group GROUP3 = getModel3();
 
+    public static void main(String[] args) {
+        DaoFactory.getInstance().createTables();
+        groupDao = new GroupDaoImpl();
+        groupDao.create(GROUP1);
+        groupDao.create(GROUP2);
+        groupDao.create(GROUP3);
+        int id = GROUP2.getId();
+        System.out.println("group: " + groupDao.findById(id));
+    }
+
+    public static Group getModel1() {
+        Group group = new Group();
+        group.setTitle("group1");
+        group.setYearEntry(Date.valueOf("2016-01-01"));
+        return group;
+    }
+
+    public static Group getModel2() {
+        Group group = new Group();
+        group.setTitle("group2");
+        group.setYearEntry(Date.valueOf("2018-01-01"));
+        return group;
+    }
+
+    public static Group getModel3() {
+        Group group = new Group();
+        group.setTitle("group3");
+        group.setYearEntry(Date.valueOf("2017-01-01"));
+        return group;
     }
 
 }
