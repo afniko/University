45fb25226813451be980aa0fa226218632e13e93
Mykola Afanasiev <afniko@gmail.com>
2019-07-28 17:38:42 +0300

    perf(V1.1_create_tables.sql): add white space #task10

--- src/main/java/ua/com/foxminded/task/dao/GroupDao.java
+++ src/main/java/ua/com/foxminded/task/dao/GroupDao.java
@@ -6,7 +6,7 @@ import ua.com.foxminded.task.domain.Group;
 
 public interface GroupDao extends CRUDDao<Group> {
 
-    public Group findByIdNoBidirectional(int id);
+    Group findByIdNoBidirectional(int id);
 
     public List<Group> findByDepartmentId(int id);
 }
