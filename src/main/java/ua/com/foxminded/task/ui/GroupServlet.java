43def707a553b2f2364daf89b4c0b5134adebdab
Mykola Afanasiev <afniko@gmail.com>
2019-08-10 12:06:57 +0300

    feat(*Servlet): add handler exceptions #task13

--- src/main/java/ua/com/foxminded/task/ui/GroupServlet.java
+++ src/main/java/ua/com/foxminded/task/ui/GroupServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 
 import ua.com.foxminded.task.dao.GroupDao;
+import ua.com.foxminded.task.dao.exception.NoEntityFoundException;
+import ua.com.foxminded.task.dao.exception.NoExecuteQueryException;
 import ua.com.foxminded.task.dao.impl.GroupDaoImpl;
 import ua.com.foxminded.task.domain.Group;
 
@@ -22,24 +24,41 @@ public class GroupServlet extends HttpServlet {
     private static final long serialVersionUID = 4603484417851175285L;
 
     private static GroupDao groupDao = new GroupDaoImpl();
-    private String text = "Group page";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = "Group page";
         List<Group> groups = null;
         String idString = req.getParameter("id");
-        if (StringUtils.isBlank(idString)) {
-            groups = groupDao.findAll();
-        } else {
-            int id = Integer.valueOf(idString);
-            groups = new ArrayList<Group>();
-            Group group = groupDao.findById(id);
-            groups.add(group);
+        try {
+            if (StringUtils.isBlank(idString)) {
+                groups = groupDao.findAll();
+            } else {
+                Group group = findGroupById(idString, req, resp);
+                groups = new ArrayList<Group>();
+                groups.add(group);
+            }
+        } catch (NoExecuteQueryException e) {
+            text = "Something with group goes wrong!";
+        } finally {
+            req.setAttribute("groups", groups);
+            req.setAttribute("text", text);
+            req.getRequestDispatcher("group.jsp").forward(req, resp);
         }
+    }
 
-        req.setAttribute("groups", groups);
-        req.setAttribute("text", text);
-        req.getRequestDispatcher("group.jsp").forward(req, resp);
+    private Group findGroupById(String idString, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Group group = null;
+        String text = null;
+        int id = Integer.valueOf(idString);
+        try {
+            group = groupDao.findById(id);
+        } catch (NoEntityFoundException e) {
+            text = "Group by id#" + id + " not found!";
+            req.setAttribute("text", text);
+            req.getRequestDispatcher("group.jsp").forward(req, resp);
+        }
+        return group;
     }
 
 }
--- src/main/java/ua/com/foxminded/task/ui/StudentServlet.java
+++ src/main/java/ua/com/foxminded/task/ui/StudentServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 
 import ua.com.foxminded.task.dao.StudentDao;
+import ua.com.foxminded.task.dao.exception.NoEntityFoundException;
+import ua.com.foxminded.task.dao.exception.NoExecuteQueryException;
 import ua.com.foxminded.task.dao.impl.StudentDaoImpl;
 import ua.com.foxminded.task.domain.Student;
 
@@ -22,24 +24,42 @@ public class StudentServlet extends HttpServlet {
     private static final long serialVersionUID = -8107642356833737724L;
 
     private static StudentDao studentDao = new StudentDaoImpl();
-    private String text = "student page";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = "Student page";
         List<Student> students = null;
-        String idString = req.getParameter("id");
-        if (StringUtils.isBlank(idString)) {
-            students = studentDao.findAll();
-        } else {
-            int id = Integer.valueOf(idString);
-            students = new ArrayList<Student>();
-            Student student = studentDao.findById(id);
-            students.add(student);
-        }
+        try {
 
-        req.setAttribute("students", students);
-        req.setAttribute("text", text);
-        req.getRequestDispatcher("student.jsp").forward(req, resp);
+            String idString = req.getParameter("id");
+            if (StringUtils.isBlank(idString)) {
+                students = studentDao.findAll();
+            } else {
+                Student student = findStudentById(idString, req, resp);
+                students = new ArrayList<Student>();
+                students.add(student);
+            }
+        } catch (NoExecuteQueryException e) {
+            text = "Something with student goes wrong!";
+        } finally {
+            req.setAttribute("students", students);
+            req.setAttribute("text", text);
+            req.getRequestDispatcher("student.jsp").forward(req, resp);
+        }
+    }
+
+    private Student findStudentById(String idString, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Student student = null;
+        String text = null;
+        int id = Integer.valueOf(idString);
+        try {
+            student = studentDao.findById(id);
+        } catch (NoEntityFoundException e) {
+            text = "Student by id#" + id + " not found!";
+            req.setAttribute("text", text);
+            req.getRequestDispatcher("student.jsp").forward(req, resp);
+        }
+        return student;
     }
 
 }
