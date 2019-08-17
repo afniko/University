84a1f62efa02672bd66ec5aa58c3ba17250c337b
Mykola Afanasiev <afniko@gmail.com>
2019-08-17 20:57:19 +0300

    test(ConverterToDtoServiceTest) create two tests #task13

--- src/main/java/ua/com/foxminded/task/domain/dto/GroupDto.java
+++ src/main/java/ua/com/foxminded/task/domain/dto/GroupDto.java
@@ -32,4 +32,35 @@ public class GroupDto {
         return yearEntry;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((yearEntry == null) ? 0 : yearEntry.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GroupDto other = (GroupDto) obj;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (yearEntry == null) {
+            if (other.yearEntry != null)
+                return false;
+        } else if (!yearEntry.equals(other.yearEntry))
+            return false;
+        return true;
+    }
+
 }
