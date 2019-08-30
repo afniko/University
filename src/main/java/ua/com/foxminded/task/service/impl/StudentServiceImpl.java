3339a40cf05a3b08f431778a35ad40c8a91b94b0
Mykola Afanasiev <afniko@gmail.com>
2019-08-30 14:50:06 +0300

    perf(StudentDaoImpl): change method update #task14

--- src/main/java/ua/com/foxminded/task/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/com/foxminded/task/service/impl/StudentServiceImpl.java
@@ -1,10 +1,14 @@
 package ua.com.foxminded.task.service.impl;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import ua.com.foxminded.task.dao.GroupDao;
 import ua.com.foxminded.task.dao.StudentDao;
+import ua.com.foxminded.task.dao.impl.GroupDaoImpl;
 import ua.com.foxminded.task.dao.impl.StudentDaoImpl;
+import ua.com.foxminded.task.domain.Group;
 import ua.com.foxminded.task.domain.Student;
 import ua.com.foxminded.task.domain.dto.StudentDto;
 import ua.com.foxminded.task.service.StudentService;
@@ -13,6 +17,7 @@ import ua.com.foxminded.task.service.converter.ConverterToDtoService;
 
 public class StudentServiceImpl implements StudentService {
     private StudentDao studentDao = new StudentDaoImpl();
+    private GroupDao groupDao = new GroupDaoImpl();
 
     public StudentServiceImpl() {
     }
@@ -41,7 +46,12 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public StudentDto update(StudentDto studentDto) {
+        Group group = null;
         Student student = ConverterFromDtoService.convert(studentDto);
+        if (Objects.nonNull(student.getGroup())) {
+            group = groupDao.findById(student.getGroup().getId());
+        }
+        student.setGroup(group);
         Student studenUpdated = studentDao.update(student);
         return ConverterToDtoService.convert(studenUpdated);
     }
