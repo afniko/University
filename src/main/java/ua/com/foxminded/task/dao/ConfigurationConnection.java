03905867098c6d9b0db97da0ed3d8d71082e7eb0
Mykola Afanasiev <afniko@gmail.com>
2019-10-23 09:46:04 +0300

    perf(StudentDaoImpl): annotation autowired not working #task16

--- src/main/java/ua/com/foxminded/task/dao/ConfigurationConnection.java
+++ src/main/java/ua/com/foxminded/task/dao/ConfigurationConnection.java
@@ -1,27 +1,21 @@
 package ua.com.foxminded.task.dao;
 
-import java.sql.Connection;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-import ua.com.foxminded.task.dao.impl.ConnectionFactoryImpl;
 
 @Configuration
-@ComponentScan
+@ComponentScan("ua.com.foxminded.task")
 public class ConfigurationConnection {
 
     @Bean
-    @Scope("singleton")
-    public ConnectionFactory getConnectionInstance() {
-        return ConnectionFactoryImpl.getInstance();
+    public DataSource getDataSource() throws NamingException {
+        InitialContext initialContext = new InitialContext();
+        return (DataSource) initialContext.lookup("java:/comp/env/jdbc/univer");
     }
-    
-    @Bean
-    @Scope("prototype")
-    public Connection getConnection() {
-        return getConnectionInstance().getConnection();
-    }
+
 }
--- src/main/java/ua/com/foxminded/task/dao/impl/StudentDaoImpl.java
+++ src/main/java/ua/com/foxminded/task/dao/impl/StudentDaoImpl.java
@@ -10,27 +10,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
-import ua.com.foxminded.task.dao.ConfigurationConnection;
-import ua.com.foxminded.task.dao.ConnectionFactory;
 import ua.com.foxminded.task.dao.StudentDao;
 import ua.com.foxminded.task.dao.exception.NoEntityFoundException;
 import ua.com.foxminded.task.dao.exception.NoExecuteQueryException;
 import ua.com.foxminded.task.domain.Group;
 import ua.com.foxminded.task.domain.Student;
 
+@Repository
 public class StudentDaoImpl implements StudentDao {
 
-    private ConnectionFactory connectionFactory;
-    private ApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationConnection.class);
+    @Autowired
+    private DataSource dataSource;
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
 
     public StudentDaoImpl() {
-        connectionFactory = context.getBean(ConnectionFactory.class);
     }
 
     @Override
@@ -43,30 +44,26 @@ public class StudentDaoImpl implements StudentDao {
 
     private Student insertPersonRecord(Student student) {
         LOGGER.debug("insertPersonRecord() [student:{}]", student);
-        String sql = "insert into persons (first_name, last_name, middle_name, birthday, idfees) " + "values (?, ?, ?, ?, ?) " + "returning id";
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = context.getBean(Connection.class);
-            preparedStatement = connection.prepareStatement(sql);
+        String sql = 
+                "insert into persons (first_name, last_name, middle_name, birthday, idfees) " 
+              + "values (?, ?, ?, ?, ?) " 
+              + "returning id";
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, student.getFirstName());
             preparedStatement.setString(2, student.getLastName());
             preparedStatement.setString(3, student.getMiddleName());
             preparedStatement.setTimestamp(4, Timestamp.valueOf(student.getBirthday().atStartOfDay()));
             preparedStatement.setInt(5, student.getIdFees());
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                student.setId(id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    student.setId(id);
+                }
             }
         } catch (SQLException e) {
-            LOGGER.error("insertPersonRecord() [student:{}] was not inserted. Sql query:{}. {}", student, preparedStatement, e);
+            LOGGER.error("insertPersonRecord() [student:{}] was not inserted. Sql query:{}. {}", student, sql, e);
             throw new NoExecuteQueryException("insertPersonRecord() Student entity was not created", e);
-        } finally {
-            connectionFactory.closeResultSet(resultSet);
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
         }
         return student;
     }
@@ -74,15 +71,12 @@ public class StudentDaoImpl implements StudentDao {
     private void insertStudentRecord(Student student) {
         LOGGER.debug("insertStudentRecord() [student:{}]", student);
         String sql = "insert into students (person_id, group_id) values (?, ?)";
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
         Integer groupId = null;
         if (Objects.nonNull(student.getGroup())) {
             groupId = student.getGroup().getId() == 0 ? null : student.getGroup().getId();
         }
-        try {
-            connection = context.getBean(Connection.class);
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, student.getId());
             if (Objects.isNull(groupId)) {
                 preparedStatement.setNull(2, java.sql.Types.INTEGER);
@@ -91,45 +85,34 @@ public class StudentDaoImpl implements StudentDao {
             }
             preparedStatement.execute();
         } catch (SQLException e) {
-            LOGGER.error("insertStudentRecord() [student:{}] was not inserted. Sql query:{}. {}.", student, preparedStatement, e);
-            throw new NoExecuteQueryException("insertStudentRecord() [student:" + student + "] was not inserted. Sql query:" + preparedStatement, e);
-        } finally {
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
+            LOGGER.error("insertStudentRecord() [student:{}] was not inserted. Sql query:{}. {}.", student, sql, e);
+            throw new NoExecuteQueryException("insertStudentRecord() [student:" + student + "] was not inserted. Sql query:" + sql, e);
         }
     }
 
     @Override
     public Student findById(int id) {
         LOGGER.debug("findById() [student id:{}]", id);
-        String sql = "select * from persons p " + "inner join students s on p.id = s.person_id " + "left join groups g on s.group_id=g.id where p.id=?";
-
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
+        String sql = 
+                "select * from persons p " 
+              + "inner join students s on p.id = s.person_id "
+              + "left join groups g on s.group_id=g.id where p.id=?";
         Student student = null;
 
-        try {
-            connection = context.getBean(Connection.class);
-
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                student = getStudentFromResultSet(resultSet);
-
-            } else {
-                LOGGER.warn("findById() Student with id#{} not found", id);
-                throw new NoEntityFoundException("Student id#" + id + "not found");
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    student = getStudentFromResultSet(resultSet);
+                } else {
+                    LOGGER.warn("findById() Student with id#{} not found", id);
+                    throw new NoEntityFoundException("Student id#" + id + "not found");
+                }
             }
         } catch (SQLException e) {
-            LOGGER.error("findById() Select Student with id#{} was crashed. Sql query:{}, {}", id, preparedStatement, e);
-            throw new NoExecuteQueryException("findById() Select Student with id#" + id + " was crashed. Sql query:" + preparedStatement, e);
-        } finally {
-            connectionFactory.closeResultSet(resultSet);
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
+            LOGGER.error("findById() Select Student with id#{} was crashed. Sql query:{}, {}", id, sql, e);
+            throw new NoExecuteQueryException("findById() Select Student with id#" + id + " was crashed. Sql query:" + sql, e);
         }
         return student;
     }
@@ -159,29 +142,22 @@ public class StudentDaoImpl implements StudentDao {
     @Override
     public List<Student> findAll() {
         LOGGER.debug("findAll()");
-        String sql = "select * from persons p " + "inner join students s on p.id = s.person_id " + "left join groups g on s.group_id=g.id";
-
+        String sql = 
+                "select * from persons p " 
+              + "inner join students s on p.id = s.person_id " 
+              + "left join groups g on s.group_id=g.id";
         List<Student> students = new ArrayList<>();
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = context.getBean(Connection.class);
-
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                students.add(getStudentFromResultSet(resultSet));
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    students.add(getStudentFromResultSet(resultSet));
+                }
             }
         } catch (SQLException e) {
-            LOGGER.error("findAll() Select all Students query was crashed. Sql query:{}, {}", preparedStatement, e);
-            throw new NoExecuteQueryException("findAll() Select all Students query was crashed. Sql query:" + preparedStatement, e);
-        } finally {
-            connectionFactory.closeResultSet(resultSet);
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
+            LOGGER.error("findAll() Select all Students query was crashed. Sql query:{}, {}", sql, e);
+            throw new NoExecuteQueryException("findAll() Select all Students query was crashed. Sql query:" + sql, e);
         }
         return students;
     }
@@ -189,30 +165,24 @@ public class StudentDaoImpl implements StudentDao {
     @Override
     public List<Student> findByGroupId(int id) {
         LOGGER.debug("findByGroupId() [id:{}]", id);
-        String sql = "select * from persons p " + "inner join students s on p.id = s.person_id " + "left join groups g on s.group_id=g.id where group_id=?";
+        String sql = 
+                "select * from persons p " 
+              + "inner join students s on p.id = s.person_id " 
+              + "left join groups g on s.group_id=g.id where group_id=?";
 
         List<Student> students = new ArrayList<>();
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = context.getBean(Connection.class);
-
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                students.add(getStudentFromResultSet(resultSet));
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    students.add(getStudentFromResultSet(resultSet));
+                }
             }
         } catch (SQLException e) {
-            LOGGER.error("findByGroupId() Select Students query by group id#{} was crashed. Sql query:{}, {}", id, preparedStatement, e);
-            throw new NoExecuteQueryException("findByGroupId()  Select Students query by group id#" + id + " was crashed. Sql query:" + preparedStatement, e);
-        } finally {
-            connectionFactory.closeResultSet(resultSet);
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
+            LOGGER.error("findByGroupId() Select Students query by group id#{} was crashed. Sql query:{}, {}", id, sql, e);
+            throw new NoExecuteQueryException("findByGroupId()  Select Students query by group id#" + id + " was crashed. Sql query:" + sql, e);
         }
         return students;
     }
@@ -228,12 +198,9 @@ public class StudentDaoImpl implements StudentDao {
     private void updatePersonRecord(Student student) {
         LOGGER.debug("updatePersonRecord() [student:{}]", student);
         String sql = "update persons set first_name=?, last_name=?, middle_name=?, birthday=?, idfees=? where id=?";
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
 
-        try {
-            connection = context.getBean(Connection.class);
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, student.getFirstName());
             preparedStatement.setString(2, student.getLastName());
             preparedStatement.setString(3, student.getMiddleName());
@@ -242,23 +209,17 @@ public class StudentDaoImpl implements StudentDao {
             preparedStatement.setInt(6, student.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            LOGGER.error("updatePersonRecord() [student:{}] was not updated. Sql query:{}. {}", student, preparedStatement, e);
+            LOGGER.error("updatePersonRecord() [student:{}] was not updated. Sql query:{}. {}", student, sql, e);
             throw new NoExecuteQueryException("updatePersonRecord() Student entity was not updated", e);
-        } finally {
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
         }
     }
 
     private void updateStudentRecord(Student student) {
         LOGGER.debug("updateStudentRecord() [student:{}]", student);
         String sql = "update students set group_id=? where person_id=? ";
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
         Integer groupId = Objects.isNull(student.getGroup()) ? null : student.getGroup().getId();
-        try {
-            connection = context.getBean(Connection.class);
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection connection = dataSource.getConnection(); 
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             if (Objects.isNull(groupId)) {
                 preparedStatement.setNull(1, java.sql.Types.INTEGER);
             } else {
@@ -267,11 +228,8 @@ public class StudentDaoImpl implements StudentDao {
             preparedStatement.setInt(2, student.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            LOGGER.error("updateStudentRecord() [student:{}] was not updated. Sql query:{}. {}.", student, preparedStatement, e);
-            throw new NoExecuteQueryException("updateStudentRecord() [student:" + student + "] was not updated. Sql query:" + preparedStatement, e);
-        } finally {
-            connectionFactory.closePreparedStatement(preparedStatement);
-            connectionFactory.closeConnection(connection);
+            LOGGER.error("updateStudentRecord() [student:{}] was not updated. Sql query:{}. {}.", student, sql, e);
+            throw new NoExecuteQueryException("updateStudentRecord() [student:" + student + "] was not updated. Sql query:" + sql, e);
         }
     }
 
