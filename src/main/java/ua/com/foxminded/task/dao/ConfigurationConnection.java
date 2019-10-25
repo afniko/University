1cf04cccabb113ac48e0c9532cfc5c2ede19fa2b
Mykola Afanasiev <afniko@gmail.com>
2019-10-25 07:29:43 +0300

    perf(ConfigurationConnection): get DataSource from JndiTemplate #task16

--- src/main/java/ua/com/foxminded/task/dao/ConfigurationConnection.java
+++ src/main/java/ua/com/foxminded/task/dao/ConfigurationConnection.java
@@ -3,7 +3,6 @@ package ua.com.foxminded.task.dao;
 import java.lang.invoke.MethodHandles;
 
 import javax.annotation.Resource;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
@@ -14,22 +13,22 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiTemplate;
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath:application.properties")
 public class ConfigurationConnection {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
 
     @Resource
     private Environment env;
-   
+
     @Bean
     public DataSource getDataSource() throws NamingException {
         LOGGER.info("ConfigurationConnection getDataSource()");
-        InitialContext initialContext = new InitialContext();
-        return (DataSource) initialContext.lookup(env.getProperty("ds.name.context"));
+        return (DataSource) new JndiTemplate().lookup(env.getProperty("ds.name.context"));
     }
 
 }
