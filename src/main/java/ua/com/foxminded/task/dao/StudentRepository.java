993913032549de6c6c59cf5b33a6363bd4a0ec00
afniko <afniko@gmail.com>
2019-12-07 20:20:48 +0200

    feat(StudentRepository): add findByGroupId(int id) #task19

--- src/main/java/ua/com/foxminded/task/dao/StudentRepository.java
+++ src/main/java/ua/com/foxminded/task/dao/StudentRepository.java
@@ -1,6 +1,9 @@
 package ua.com.foxminded.task.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ua.com.foxminded.task.domain.Student;
@@ -8,4 +11,6 @@ import ua.com.foxminded.task.domain.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    @Query("SELECT s FROM Student s WHERE s.group.id = ?1")
+    public List<Student> findByGroupId(Integer id);
 }
