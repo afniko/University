7fdfa333da4d247aeda1c754b2bd45395f98333b
afniko <afniko@gmail.com>
2019-12-07 19:47:29 +0200

    perf(*Repository): add annotation Repository #task19

--- src/main/java/ua/com/foxminded/task/dao/StudentRepository.java
+++ src/main/java/ua/com/foxminded/task/dao/StudentRepository.java
@@ -1,9 +1,11 @@
 package ua.com.foxminded.task.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ua.com.foxminded.task.domain.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
