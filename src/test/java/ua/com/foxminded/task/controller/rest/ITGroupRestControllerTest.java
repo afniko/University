74b8744af352dd315a50962a9124ebd6dcf185aa
afniko <afniko@gmail.com>
2019-12-23 23:40:05 +0200

    test(ITGroupControllerTest): add rest tests #task21

--- src/test/java/ua/com/foxminded/task/controller/rest/ITGroupRestControllerTest.java
+++ src/test/java/ua/com/foxminded/task/controller/rest/ITGroupRestControllerTest.java
@@ -1,14 +1,13 @@
 package ua.com.foxminded.task.controller.rest;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import java.util.Arrays;
-import java.util.List;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.AfterEach;
@@ -18,15 +17,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import ua.com.foxminded.task.dao.GroupRepository;
 import ua.com.foxminded.task.domain.Group;
-import ua.com.foxminded.task.domain.dto.GroupDto;
 import ua.com.foxminded.task.domain.repository.GroupModelRepository;
-import ua.com.foxminded.task.domain.repository.dto.GroupDtoModelRepository;
 
 @SpringBootTest
 public class ITGroupRestControllerTest {
@@ -36,23 +32,8 @@ public class ITGroupRestControllerTest {
     @Autowired
     private GroupRepository groupRepository;
 
-//    private static final String PATH_HTML_GROUP = "group/group";
-    private static final String PATH_HTML_GROUPS = "group/groups";
-//    private static final String PATH_HTML_GROUP_EDIT = "group/group_edit";
-    private static final String ATTRIBUTE_HTML_TITLE = "title";
-//    private static final String ATTRIBUTE_HTML_GROUP = "groupDto";
-    private static final String ATTRIBUTE_HTML_GROUPS = "groups";
-//    private static final String ATTRIBUTE_HTML_SUCCESS_MESSAGE = "successMessage";
-//    private static final String ATTRIBUTE_HTML_ERROR_MESSAGE = "errorMessage";
-//    private static final String EXPECTED_ERROR_MESSAGE = "You enter incorrect data!";
     private static final Group GROUP1 = GroupModelRepository.getModel1();
     private static final Group GROUP2 = GroupModelRepository.getModel2();
-    private static final Group GROUP3 = GroupModelRepository.getModel3();
-    private static final Group GROUP4 = GroupModelRepository.getModel4();
-    private static final GroupDto GROUP_DTO1 = GroupDtoModelRepository.getModel1();
-    private static final GroupDto GROUP_DTO2 = GroupDtoModelRepository.getModel2();
-    private static final GroupDto GROUP_DTO3 = GroupDtoModelRepository.getModel3();
-    private static final GroupDto GROUP_DTO4 = GroupDtoModelRepository.getModel4();
 
     private MockMvc mockMvc;
 
@@ -61,152 +42,106 @@ public class ITGroupRestControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
         flyway.migrate();
         groupRepository.save(GROUP1);
-        groupRepository.save(GROUP2);
-        groupRepository.save(GROUP3);
-        groupRepository.saveAndFlush(GROUP4);
+        groupRepository.saveAndFlush(GROUP2);
     }
 
     @Test
     void whenRetriveAllGroups_thenExpectListOfGroups() throws Exception {
-        List<GroupDto> groups = Arrays.asList(GROUP_DTO1, GROUP_DTO2, GROUP_DTO3);
-        MvcResult mvcResult =  this.mockMvc.perform(get("/groups").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/api/groups").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(view().name(PATH_HTML_GROUPS))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andReturn();
-        String expectedTitle = "Groups";
-        String actuallyTitle = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_TITLE).toString();
-        assertEquals(actuallyTitle, expectedTitle);
-        List<GroupDto> actuallyGroups = (List<GroupDto>) mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_GROUPS);
-        assertTrue(actuallyGroups.containsAll(groups));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("group1")))
+                .andExpect(jsonPath("$[0].yearEntry", is(2016)))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].title", is("group2")))
+                .andExpect(jsonPath("$[1].yearEntry", is(2018)));
     }
 
-//  @Test
-//  void whenRetriveTheGroup_thenExpectGroupById() throws Exception {
-//      GroupDto groupDto = GroupDtoModelRepository.getModelWithId();
-//      int id = groupDto.getId();
-//      String httpRequest = "/group?id=" + id;
-//      MvcResult mvcResult = this.mockMvc.perform(get(httpRequest).accept(MediaType.TEXT_HTML_VALUE))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP))
-//              .andDo(print())
-//              .andReturn();
-//      String expectedTitle = "Group";
-//      String actuallyTitle = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_TITLE).toString();
-//      assertEquals(actuallyTitle, expectedTitle);
-//      GroupDto actuallyGroup = (GroupDto) mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_GROUP);
-//      assertEquals(groupDto, actuallyGroup);
-//  }
-//
-//  @Test
-//  void whenRetriveEditExistsGroup_thenExpectFormWithGroupField() throws Exception {
-//      GroupDto groupDto = GroupDtoModelRepository.getModelWithId();
-//      int id = groupDto.getId();
-//      String httpRequest = "/group_edit?id=" + id;
-//      MvcResult mvcResult = this.mockMvc.perform(get(httpRequest).accept(MediaType.TEXT_HTML_VALUE))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP_EDIT))
-//              .andDo(print())
-//              .andReturn();
-//      String expectedTitle = "Group edit";
-//      String actuallyTitle = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_TITLE).toString();
-//      assertEquals(actuallyTitle, expectedTitle);
-//      GroupDto actuallyGroup = (GroupDto) mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_GROUP);
-//      assertEquals(groupDto, actuallyGroup);
-//  }
-//
-//  @Test
-//  void whenSubmitEditFormGroupWithId_thenUpdateGroup() throws Exception {
-//      GroupDto groupDto = GroupDtoModelRepository.getModelWithId();
-//      String httpRequest = "/group_edit";
-//      MvcResult mvcResult = this.mockMvc.perform(post(httpRequest).accept(MediaType.TEXT_HTML_VALUE).flashAttr("groupDto", groupDto))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP))
-//              .andDo(print())
-//              .andReturn();
-//      String expectedTitle = "Group edit";
-//      String actuallyTitle = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_TITLE).toString();
-//      assertEquals(actuallyTitle, expectedTitle);
-//      GroupDto actuallyGroup = (GroupDto) mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_GROUP);
-//      assertEquals(groupDto, actuallyGroup);
-//      String expectedSuccessMessage = "Record group was updated!";
-//      String actuallySuccessMessage = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_SUCCESS_MESSAGE).toString();
-//      assertEquals(actuallySuccessMessage, expectedSuccessMessage);
-//  }
-//
-//  @Test
-//  void whenSubmitEditFormGroupWithoutId_thenCreateGroup() throws Exception {
-//      GroupDto groupDto = GroupDtoModelRepository.getModel6();
-//      String httpRequest = "/group_edit";
-//      MvcResult mvcResult = this.mockMvc.perform(post(httpRequest).accept(MediaType.TEXT_HTML_VALUE).flashAttr("groupDto", groupDto))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP))
-//              .andDo(print())
-//              .andReturn();
-//      String expectedTitle = "Group edit";
-//      String actuallyTitle = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_TITLE).toString();
-//      assertEquals(actuallyTitle, expectedTitle);
-//      GroupDto actuallyGroup = (GroupDto) mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_GROUP);
-//      assertEquals(groupDto, actuallyGroup);
-//      String expectedSuccessMessage = "Record group was created!";
-//      String actuallySuccessMessage = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_SUCCESS_MESSAGE).toString();
-//      assertEquals(actuallySuccessMessage, expectedSuccessMessage);
-//  }
-//  
-//  @Test
-//  void whenUpdateGroupWithNotCorrectValues_thenExpectError() throws Exception {
-//      GroupDto groupDto = GROUP_DTO4;
-//      groupDto.setId(1);
-//      groupDto.setTitle(GROUP_DTO2.getTitle());
-//      groupDto.setYearEntry(99999);
-//      String httpRequest = "/group_edit";
-//      MvcResult mvcResult = this.mockMvc.perform(post(httpRequest).accept(MediaType.TEXT_HTML_VALUE).flashAttr("groupDto", groupDto))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP_EDIT))
-//              .andExpect(model().attributeHasFieldErrorCode("groupDto", "title", "GroupTitleUnique"))
-//              .andExpect(model().attributeHasFieldErrorCode("groupDto", "yearEntry", "Max"))
-//              .andDo(print())
-//              .andReturn();
-//        
-//        String actuallyErrorMessage = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE).toString();
-//        assertEquals(EXPECTED_ERROR_MESSAGE, actuallyErrorMessage);
-//    }
-//  
-//  @Test
-//  void whenUpdateGroupWithNotCorrectValues_thenExpectError2() throws Exception {
-//      GroupDto groupDto = GROUP_DTO4;
-//      groupDto.setId(1);
-//      groupDto.setTitle("qwertyuiopasdfghjklzxcvbnm");
-//      groupDto.setYearEntry(999);
-//      String httpRequest = "/group_edit";
-//      MvcResult mvcResult = this.mockMvc.perform(post(httpRequest).accept(MediaType.TEXT_HTML_VALUE).flashAttr("groupDto", groupDto))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP_EDIT))
-//              .andExpect(model().attributeHasFieldErrorCode("groupDto", "yearEntry", "Min"))
-//              .andExpect(model().attributeHasFieldErrorCode("groupDto", "title", "Length"))
-//              .andDo(print())
-//              .andReturn();
-//      
-//      String actuallyErrorMessage = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE).toString();
-//      assertEquals(EXPECTED_ERROR_MESSAGE, actuallyErrorMessage);
-//  }
-//  
-//  @Test
-//  void whenUpdateGroupWithNotCorrectTitle_thenExpectError() throws Exception {
-//      GroupDto groupDto = GROUP_DTO4;
-//      groupDto.setId(1);
-//      groupDto.setTitle("");
-//      String httpRequest = "/group_edit";
-//      MvcResult mvcResult = this.mockMvc.perform(post(httpRequest).accept(MediaType.TEXT_HTML_VALUE).flashAttr("groupDto", groupDto))
-//              .andExpect(status().isOk())
-//              .andExpect(view().name(PATH_HTML_GROUP_EDIT))
-//              .andExpect(model().attributeHasFieldErrorCode("groupDto", "title", "NotBlank"))
-//              .andDo(print())
-//              .andReturn();
-//      
-//      String actuallyErrorMessage = mvcResult.getRequest().getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE).toString();
-//      assertEquals(EXPECTED_ERROR_MESSAGE, actuallyErrorMessage);
-//  }
+    @Test
+    void whenRetriveTheGroup_thenExpectGroupById() throws Exception {
+        this.mockMvc.perform(get("/api/groups/2").accept(MediaType.APPLICATION_JSON))
+                  .andExpect(status().isOk())
+                  .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                  .andDo(print())
+                  .andExpect(jsonPath("$", aMapWithSize(3)))
+                  .andExpect(jsonPath("$.id", is(2)))
+                  .andExpect(jsonPath("$.title", is("group2")))
+                  .andExpect(jsonPath("$.yearEntry", is(2018)));
+    }
+
+  @Test
+  void whenSubmitEditFormGroupWithId_thenUpdateGroup() throws Exception {
+      String group = "{\"id\":2,\"title\":\"group2\",\"yearEntry\":2001}";
+      this.mockMvc.perform(post("/api/groups")
+                .content(group)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", aMapWithSize(3)))
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.title", is("group2")))
+                .andExpect(jsonPath("$.yearEntry", is(2001)));
+  }
+
+  @Test
+  void whenSubmitEditFormGroupWithoutId_thenCreateGroup() throws Exception {
+      String group = "{\"id\":0,\"title\":\"group3\",\"yearEntry\":2013}";
+      this.mockMvc.perform(post("/api/groups")
+                .content(group)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", aMapWithSize(3)))
+                .andExpect(jsonPath("$.id", is(3)))
+                .andExpect(jsonPath("$.title", is("group3")))
+                .andExpect(jsonPath("$.yearEntry", is(2013)));
+  }
+ 
+  @Test
+  void whenUpdateGroupWithNotCorrectValues_thenExpectError() throws Exception {
+      String group = "{\"id\":2,\"title\":\"group1\",\"yearEntry\":99999}";
+      this.mockMvc.perform(post("/api/groups")
+                .content(group)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.errors", aMapWithSize(2)))
+                .andExpect(jsonPath("$.errors.title", is("Title is already exists!")))
+                .andExpect(jsonPath("$.errors.yearEntry", is("Year of entry is not correct!")));
+  }
+  
+  @Test
+  void whenUpdateGroupWithNotCorrectValues_thenExpectError2() throws Exception {
+      String group = "{\"id\":2,\"title\":\"qwertyuiopasdfghjklzxcvbnm\",\"yearEntry\":999}";
+      this.mockMvc.perform(post("/api/groups")
+                .content(group)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.errors", aMapWithSize(2)))
+                .andExpect(jsonPath("$.errors.title", is("Maximum length is 20!")))
+                .andExpect(jsonPath("$.errors.yearEntry", is("Year of entry is not correct!")));
+  }
+  
+  @Test
+  void whenUpdateGroupWithNotCorrectTitle_thenExpectError() throws Exception {
+      String group = "{\"id\":2,\"title\":\"\",\"yearEntry\":2018}";
+      this.mockMvc.perform(post("/api/groups")
+                .content(group)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.errors", aMapWithSize(1)))
+                .andExpect(jsonPath("$.errors.title", is("Title can`t be blank!")));
+  }
   
     @AfterEach
     public void removeCreatedTables() {
