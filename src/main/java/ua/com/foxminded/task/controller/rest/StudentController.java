389729b316928297a666ffb32f33152d9c9c3175
afniko <afniko@gmail.com>
2019-12-25 19:05:26 +0200

    perf(*Controller, *Dto): add description for swagger #task22

--- src/main/java/ua/com/foxminded/task/controller/rest/StudentController.java
+++ src/main/java/ua/com/foxminded/task/controller/rest/StudentController.java
@@ -13,11 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import ua.com.foxminded.task.domain.dto.StudentDto;
 import ua.com.foxminded.task.service.StudentService;
 
 @RestController("studentRestController")
 @RequestMapping("/api")
+@Api(description = "Student management System", produces = "application/json", consumes = "application/json")
 public class StudentController {
 
     private Logger logger;
@@ -30,18 +33,21 @@ public class StudentController {
     }
 
     @GetMapping(path = "/students", produces = "application/json")
+    @ApiOperation(value = "View a list of available students")
     public List<StudentDto> getStudents() {
         logger.debug("students()");
         return studentService.findAllDto();
     }
 
     @GetMapping(path = "/students/{id}", produces = "application/json")
+    @ApiOperation(value = "View a student by id")
     public StudentDto getStudentById(@PathVariable("id") int id) {
         logger.debug("studentById()");
         return studentService.findByIdDto(id);
     }
 
     @PostMapping(path = "/students", produces = "application/json")
+    @ApiOperation(value = "Create (if id=0) or update a student")
     public StudentDto saveStudent(@Valid @RequestBody StudentDto studentDto) {
         logger.debug("editPost()");
         if (studentDto.getId() != 0) {
