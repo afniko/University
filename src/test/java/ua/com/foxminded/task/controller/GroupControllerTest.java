ef4dbdbc8211e910021230f5cf1912b93b33740f
afniko <afniko@gmail.com>
2020-01-15 20:02:02 +0200

    test(GroupControllerTest): add tests #task23

--- src/test/java/ua/com/foxminded/task/controller/GroupControllerTest.java
+++ src/test/java/ua/com/foxminded/task/controller/GroupControllerTest.java
@@ -1,4 +1,5 @@
 package ua.com.foxminded.task.controller;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -21,7 +22,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
+import ua.com.foxminded.task.dao.exception.EntityAlreadyExistsException;
+import ua.com.foxminded.task.dao.exception.EntityNotValidException;
 import ua.com.foxminded.task.domain.dto.GroupDto;
 import ua.com.foxminded.task.domain.repository.dto.GroupDtoModelRepository;
 import ua.com.foxminded.task.service.GroupService;
@@ -38,12 +44,16 @@ public class GroupControllerTest {
     @MockBean
     private Logger logger;
     
+    @MockBean
+    private BindingResult bindingResult;
+    
     private static final String PATH_HTML_GROUP = "group/group";
     private static final String PATH_HTML_GROUPS = "group/groups";
     private static final String PATH_HTML_GROUP_EDIT = "group/group_edit";
     private static final String ATTRIBUTE_HTML_GROUP = "groupDto";
     private static final String ATTRIBUTE_HTML_GROUPS = "groups";
     private static final String ATTRIBUTE_HTML_ERROR_MESSAGE = "errorMessage";
+    private static final String ATTRIBUTE_HTML_SUCCESS_MESSAGE = "successMessage";
     private static final String ATTRIBUTE_HTML_TITLE = "title";
     
     @BeforeEach
@@ -168,5 +178,85 @@ public class GroupControllerTest {
                 .andExpect(model().attribute(ATTRIBUTE_HTML_ERROR_MESSAGE, equalTo(expectedErrorMessage)))
                 .andExpect(forwardedUrl(PATH_HTML_GROUP_EDIT))
                 .andDo(print());
+    }
+    
+    @Test
+    void whenSubmitEditFormGroupWithId_thenUpdateGroup() throws Exception {
+        GroupDto groupDto = GroupDtoModelRepository.getModelWithId();
+        String expectedTitle = "Group edit";
+        String expectedSuccessMessage = "Record group was updated!";
+        Model model = new ExtendedModelMap();
+
+        when(bindingResult.hasErrors()).thenReturn(false);
+        when(groupService.update(groupDto)).thenReturn(groupDto);
+
+        String actuallyView = groupController.editPost(groupDto, bindingResult, model);
+        
+        assertThat(PATH_HTML_GROUP).isEqualTo(actuallyView);
+        assertThat(expectedTitle).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_TITLE));
+        assertThat(expectedSuccessMessage).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_SUCCESS_MESSAGE));
+        assertThat(groupDto).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_GROUP));
+    }
+
+    @Test
+    void whenSubmitEditFormGroupWithoutId_thenCreateGroup() throws Exception {
+        GroupDto groupDto = GroupDtoModelRepository.getModel6();
+        String expectedTitle = "Group edit";
+        String expectedSuccessMessage = "Record group was created!";
+        Model model = new ExtendedModelMap();
+
+        when(groupService.create(groupDto)).thenReturn(groupDto);
+
+        String actuallyView = groupController.editPost(groupDto, bindingResult, model);
+
+        assertThat(PATH_HTML_GROUP).isEqualTo(actuallyView);
+        assertThat(expectedTitle).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_TITLE));
+        assertThat(expectedSuccessMessage).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_SUCCESS_MESSAGE));
+        assertThat(groupDto).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_GROUP));
+    }
+    
+    @Test
+    void whenCreateExistsGroup_thenExpectErrorMessage() throws Exception {
+        GroupDto groupDto = GroupDtoModelRepository.getModel1();
+        String expectedErrorMessage = "Record group was not created! The record already exists!";
+        Model model = new ExtendedModelMap();
+        
+        doThrow(EntityAlreadyExistsException.class).when(groupService).create(groupDto);
+      
+        String actuallyView = groupController.editPost(groupDto, bindingResult, model);
+
+        assertThat(PATH_HTML_GROUP_EDIT).isEqualTo(actuallyView);
+        assertThat(expectedErrorMessage).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE));
+        assertThat(groupDto).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_GROUP));
+    }
+  
+    @Test
+    void whenUpdateNotFountEntity_thenExpectErrorMessage() throws Exception {
+        GroupDto groupDto = GroupDtoModelRepository.getModelWithId();
+        String expectedErrorMessage = "Group " + groupDto + " not found!";
+        Model model = new ExtendedModelMap();
+      
+        doThrow(EntityNotFoundException.class).when(groupService).update(groupDto);
+      
+        String actuallyView = groupController.editPost(groupDto, bindingResult, model);
+
+        assertThat(PATH_HTML_GROUP_EDIT).isEqualTo(actuallyView);
+        assertThat(expectedErrorMessage).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE));
+        assertThat(groupDto).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_GROUP));
+    }
+  
+    @Test
+    void whenCreateNotValidEntity_thenExpectErrorMessage() throws Exception {
+        GroupDto groupDto = GroupDtoModelRepository.getModel1();
+        String expectedErrorMessage = "Record group was not updated/created! The data is not valid!";
+        Model model = new ExtendedModelMap();
+      
+        doThrow(EntityNotValidException.class).when(groupService).create(groupDto);
+      
+        String actuallyView = groupController.editPost(groupDto, bindingResult, model);
+
+        assertThat(PATH_HTML_GROUP_EDIT).isEqualTo(actuallyView);
+        assertThat(expectedErrorMessage).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_ERROR_MESSAGE));
+        assertThat(groupDto).isEqualTo(model.getAttribute(ATTRIBUTE_HTML_GROUP));
     }
 }
