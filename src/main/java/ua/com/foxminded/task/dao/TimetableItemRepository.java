60049c4ccdc0c619be6d0f70b5c2009e41f47b29
afniko <afniko@gmail.com>
2020-01-31 20:20:29 +0200

    feat(TimetableItemRepository): add repo #task106 #6

---
+++ src/main/java/ua/com/foxminded/task/dao/TimetableItemRepository.java
@@ -0,0 +1,15 @@
+package ua.com.foxminded.task.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ua.com.foxminded.task.domain.Teacher;
+
+@Repository
+@Transactional
+public interface TimetableItemRepository extends JpaRepository<Teacher, Integer> {
+
+    public boolean existsById(Integer id);
+
+}
