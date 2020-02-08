d86bfc80edd85b38cf9ba43d37a4e6d8c78b2611
afniko <afniko@gmail.com>
2020-02-08 14:00:05 +0200

    feat(add_records.sql): add records #task105 #5

--- src/main/resources/add_records.sql
+++ src/main/resources/add_records.sql
@@ -1,4 +1,4 @@
-  INSERT INTO groups (title, department_id, yearEntry) VALUES
+﻿  INSERT INTO groups (title, department_id, yearEntry) VALUES
     ('G151', 1, 2015),
     ('G152', 1, 2015),
     ('G153', 1, 2015),
@@ -52,6 +52,7 @@
     ('department3', 'bla bla bla 3', 1),
     ('department4', 'bla bla bla 4', 2),
     ('department5', 'bla bla bla 5', 2);
+
 
   INSERT INTO persons (first_name, last_name, middle_name, birthday, idfees) VALUES
     ('firstname1', 'lastname1', 'middlename1', '1991-02-01', 111111111),
@@ -131,3 +132,29 @@
     ('103a', 4, 30, 'bla bla bla 4'),
     ('405a', 1, 14, 'bla bla bla 5'),
     ('161a', 2, 20, 'bla bla bla 6');
+
+  INSERT INTO timetable_items (subject_id, auditory_id, lecture_id, date, teacher_id) VALUES
+    (1, 1, 1, '2020-06-25', 1),
+    (2, 2, 1, '2020-06-25', 2),
+    (2, 2, 2, '2020-06-25', 2),
+    (3, 3, 2, '2020-06-25', 3),
+    (3, 3, 3, '2020-06-25', 3),
+    (4, 4, 1, '2020-06-25', 3),
+    (4, 4, 2, '2020-06-25', 3),
+    (1, 5, 5, '2020-06-25', 1),
+    (2, 4, 5, '2020-06-25', 2),
+    (3, 3, 6, '2020-06-25', 3);
+
+  INSERT INTO groups_timetable_items (group_id, timetable_item_id) VALUES
+    (1, 1),
+    (1, 4),
+    (1, 9),
+    (2, 1),
+    (2, 3),
+    (2, 5),
+    (2, 6),
+    (3, 2),
+    (3, 7),
+    (4, 2),
+    (4, 8),
+    (4, 10);
